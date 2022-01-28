8b22aeb8f4592ffb8b7404102214bb3b67dd8ba1
Nico Tippmann <nico.tippmann@megware.com>
2022-01-28 14:08:50 +0100

    add port to build args

--- container/trex_server/install.sh
+++ container/trex_server/install.sh
@@ -12,10 +12,12 @@ function print_help {
     echo -e "-h|--help\tprints this"
     echo ""
     echo -e "-e|--executor\t[docker|podman] executer (default: podman)"
+    echo -e "-p|--port\t Port (default: 7100)"
 }
 
 SERVICE_NAME="trex_server"
 EXECUTOR="podman"
+PORT="7100"
 while [[ $# -gt 1 ]]; do
     key="$1"
     echo $key
@@ -29,13 +31,18 @@ while [[ $# -gt 1 ]]; do
         shift
         shift
         ;;
+        -p|--port)
+        PORT="$2"
+        shift
+        shift
+        ;;
     esac
 done
 
 INSTALL_PATH="/usr/local/share/${SERVICE_NAME}/container"
 mkdir -p $INSTALL_PATH
 
-${EXECUTOR} build --network=host --no-cache -t ${SERVICE_NAME}_container -f ./Dockerfile ../../
+${EXECUTOR} build --network=host --build-arg PORT=${PORT} --no-cache -t ${SERVICE_NAME}_container -f ./Dockerfile ../../
 
 cp 	${SERVICE_NAME}.service /etc/systemd/system/
 systemctl daemon-reload
