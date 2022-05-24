7d595bb17f02a09182fde41e5c5b196ec518de54
Kevin Thomas <kevin.thomas@megware.com>
2022-05-24 11:00:33 +0200

    add y_combinator storing functor/lambda in shared_ptr -> lifetime managed for event loop, allows using mutable captures without copy instead of shared_ptr

--- src/proxy/y_combinator.h
+++ src/proxy/y_combinator.h
@@ -7,6 +7,9 @@
 namespace cw {
 namespace helper {
 
+/**
+ * @brief Ycombinator storing functor by copying.
+ */
 template <class F>
 class y_combinator_result {
 private:
@@ -44,6 +47,27 @@ template <class Fun>
 y_combinator_result<std::decay_t<Fun>> y_combinator(Fun&& f) {
     return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(f));
 }
+
+/**
+ * @brief Ycombinator storing functor in shared_ptr.
+ */
+template <class F>
+class y_combinator_shared_result {
+private:
+    std::shared_ptr<F> f;
+public:
+    explicit y_combinator_shared_result(F &&fun): f(std::shared_ptr<F>{new F(std::forward<F>(fun))}) {}
+    template <class... Args>
+    decltype(auto) operator()(Args&&... args) const { return (*f)(*this, std::forward<Args>(args)...); }
+    template <class... Args>
+    decltype(auto) operator()(Args&&... args) { return (*f)(*this, std::forward<Args>(args)...); }
+};
+
+template <class Fun>
+y_combinator_shared_result<std::decay_t<Fun>> y_combinator_shared(Fun&& f) {
+    return y_combinator_shared_result<std::decay_t<Fun>>{std::forward<Fun>(f)};
+}
+
 
 }
 }
